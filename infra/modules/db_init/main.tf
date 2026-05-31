1ce846680f6130e0fef5480382770864c5b9f37c
Krishna <rkishtiaha5@gmail.com>
2026-05-31 18:06:45 +0530

    dms cloud-watch role fixed

--- infra/modules/db_init/main.tf
+++ infra/modules/db_init/main.tf
@@ -2,22 +2,17 @@ resource "null_resource" "create_databases" {
 
   provisioner "local-exec" {
 
-    command = <<EOT
-mysql \
--h ${var.rds_host} \
--u ${var.db_user} \
--p${var.db_password} \
+    interpreter = ["/bin/bash", "-c"]
+
+    command = <<-EOT
+mysql -h "${var.rds_host}" \
+-u "${var.db_user}" \
+-p"${var.db_password}" \
 -e "
 CREATE DATABASE IF NOT EXISTS ${var.raw_db_name};
 CREATE DATABASE IF NOT EXISTS ${var.replicated_db_name};
 CREATE DATABASE IF NOT EXISTS ${var.unified_db_name};
 "
 EOT
-  }
-
-  triggers = {
-    raw_db        = var.raw_db_name
-    replicated_db = var.replicated_db_name
-    unified_db    = var.unified_db_name
   }
 }
