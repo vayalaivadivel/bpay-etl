b38200a0c35f72328901cb0bd3e57556968e0f74
Vadivel M <vadivel.m@collinsongroup.com>
2026-06-27 22:57:19 +0530

    Restore Route53 prevent_destroy after infrastructure cleanup

--- infra/modules/route53/main.tf
+++ infra/modules/route53/main.tf
@@ -2,9 +2,9 @@ resource "aws_route53_zone" "this" {
 
   name = var.domain_name
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  #lifecycle {
+  #  prevent_destroy = true
+  #}
 
   tags = {
     Name        = "${var.app_name}-dns-${var.env}"
