9adf8ac21a17d051ab43baa267b7416d7ef6b9e1
Vadivel M <vadivel.m@collinsongroup.com>
2026-07-15 16:43:46 +0530

    Provisioned

--- infra/modules/ec2/bootstrap.tf
+++ infra/modules/ec2/bootstrap.tf
@@ -21,7 +21,7 @@ resource "terraform_data" "bootstrap" {
 
   provisioner "file" {
     source      = "${path.module}/scripts"
-    destination = "/opt/bpay"
+    destination = "/home/ubuntu"
   }
 
   #############################################
@@ -30,35 +30,44 @@ resource "terraform_data" "bootstrap" {
 
   provisioner "file" {
     source      = "${path.module}/init-databases.sql.tpl"
-    destination = "/opt/bpay/sql/init-databases.sql"
+    destination = "/home/ubuntu/init-databases.sql"
   }
 
   provisioner "file" {
     source      = "${path.module}/init-source.sql.tpl"
-    destination = "/opt/bpay/sql/init-source.sql"
+    destination = "/home/ubuntu/init-source.sql"
   }
 
   provisioner "file" {
     source      = "${path.module}/init-replicated.sql.tpl"
-    destination = "/opt/bpay/sql/init-replicated.sql"
+    destination = "/home/ubuntu/init-replicated.sql"
   }
 
   provisioner "file" {
     source      = "${path.module}/init-unified.sql.tpl"
-    destination = "/opt/bpay/sql/init-unified.sql"
+    destination = "/home/ubuntu/init-unified.sql"
   }
 
   #############################################
   # INITIALIZE DATABASE
   #############################################
-
   provisioner "remote-exec" {
 
     inline = [
       <<-EOT
 set -e
 
-chmod +x /opt/bpay/scripts/*.sh
+sudo mkdir -p /opt/bpay/scripts
+sudo mkdir -p /opt/bpay/sql
+
+sudo cp -r /home/ubuntu/scripts/* /opt/bpay/scripts/
+
+sudo cp /home/ubuntu/init-databases.sql /opt/bpay/sql/
+sudo cp /home/ubuntu/init-source.sql /opt/bpay/sql/
+sudo cp /home/ubuntu/init-replicated.sql /opt/bpay/sql/
+sudo cp /home/ubuntu/init-unified.sql /opt/bpay/sql/
+
+sudo chmod +x /opt/bpay/scripts/*.sh
 
 export RDS_HOST="${split(":", var.rds_endpoint)[0]}"
 export DB_USER="${var.db_username}"
@@ -67,13 +76,12 @@ export DB_NAME="${var.db_name}"
 export REPLICATED_DB_NAME="${var.replicated_db_name}"
 export UNIFIED_DB_NAME="${var.unified_db_name}"
 
-echo "Initializing database..."
 bash /opt/bpay/scripts/init-db.sh
 
-echo "Verifying database..."
 bash /opt/bpay/scripts/verify-db.sh
 
 echo "Bootstrap completed successfully."
+
 EOT
     ]
   }
