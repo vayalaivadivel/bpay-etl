f7fe640ba277563c882ff05f66aead82b82376fe
Vadivel M <vadivel.m@collinsongroup.com>
2026-07-23 16:22:18 +0530

    Validate

--- infra/modules/ec2/bootstrap.tf
+++ infra/modules/ec2/bootstrap.tf
@@ -1,7 +1,11 @@
 resource "null_resource" "bootstrap" {
 
   depends_on = [
-    aws_instance.bastion
+    aws_instance.bastion,
+    local_file.init_databases,
+    local_file.init_source,
+    local_file.init_replicated,
+    local_file.init_unified
   ]
 
   triggers = {
@@ -25,30 +29,36 @@ resource "null_resource" "bootstrap" {
   }
 
   #############################################
-  # COPY SQL FILES
+  # COPY RENDERED SQL
   #############################################
+
   provisioner "file" {
-    source      = "${path.module}/rendered"
+    source      = "${path.root}/.terraform-rendered/ec2"
     destination = "/home/ubuntu"
   }
 
   #############################################
   # INITIALIZE DATABASE
   #############################################
+
   provisioner "remote-exec" {
     inline = [
       "sudo mkdir -p /opt/bpay/scripts",
       "sudo mkdir -p /opt/bpay/sql",
 
       "sudo cp -r /home/ubuntu/scripts/* /opt/bpay/scripts/",
-      "sudo cp -r /home/ubuntu/rendered/* /opt/bpay/sql/",
+      "sudo cp -r /home/ubuntu/ec2/* /opt/bpay/sql/",
 
       "sudo chmod +x /opt/bpay/scripts/*.sh",
 
+      "echo '===== Scripts ====='",
       "ls -lrt /opt/bpay/scripts",
+
+      "echo '===== SQL Files ====='",
       "ls -lrt /opt/bpay/sql",
 
       "bash /opt/bpay/scripts/init-db.sh",
+
       "bash /opt/bpay/scripts/verify-db.sh"
     ]
   }
